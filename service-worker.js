eb4cbbb7a0a80bf3dc408b0fe0aaf5428d345c52
Eran Hsu <46525202+eran0926@users.noreply.github.com>
2024-07-31 00:03:58 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,13 @@
+const cacheName = "scoutPWA-v1";
+const contentToCache = ["/index.html"]
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })(),
+  );
+});
